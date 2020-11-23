5ae0e8e82c579f0ab3b669ad9f0312eff4ab616f
crashdaddy <kennethscoggins@mail.com>
2020-11-23 05:55:10 -0600

    edit about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,9 +6,14 @@ class About extends Component {
     render() {
 
         return (
-          <div style={{marginTop:'100px'}}>
-              himom
-           <img src='MediaBiasChart.jpg' style={{width:'90%'}} />
+          <div style={{marginTop:'100px',fontSize:'30pt',padding:'40px'}}>
+           Newshound combines RSS XML newsfeeds from various news agencies
+           with agendas across the political spectrum and displays them
+           side-by-side for the discerning political observer.<br/>
+           <p style={{fontSize:'small',textDecoration:'italic'}}>
+               Biases based on the following criteria:
+           </p>   
+           <img src='MediaBiasChart.jpg' style={{width:'90%',marginTop:'40px'}} />
           </div>
         )
       }
